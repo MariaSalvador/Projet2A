1af00e349c068bcc141e0dc1990e21611fd55136
stanciopaul <paul.stancioni6@etu.univ-lorraine.fr>
2018-04-09 11:57:01 +0200

    commit 09/04

--- REST_Terminator/src/java/serveur/TerminatorResource.java
+++ REST_Terminator/src/java/serveur/TerminatorResource.java
@@ -1,25 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package serveur;
 
-
-
-
+import coppelia.BoolW;
+import coppelia.FloatWA;
 import coppelia.IntW;
 import coppelia.IntWA;
-import coppelia.FloatWA;
 import coppelia.remoteApi;
-import coppelia.BoolW;
-
-import java.io.*;
-import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import org.json.JSONObject;
-
-
+import java.net.URL;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -28,20 +18,15 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
-//import javax.ws.rs.QueryParam;
-
-
-
+import org.json.JSONObject;
 /**
  * REST Web Service
  *
  * @author PC_fixe_1
  */
-
-public class Cubot2 {
-
+@Path("Terminator")
+public class TerminatorResource {
     
-    protected remoteApi vrep;
     protected int clientID;
     int answer;
     IntWA objectHandles = new IntWA(0);
@@ -58,107 +43,78 @@ public class Cubot2 {
     BoolW result =new BoolW(false);
     int codeRetour;
     float temps;
-    
-    public static void main(String[] args) {
-        
-        
-        new Cubot2();
+    remoteApi vrep = new remoteApi();
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of TerminatorResource
+     */
+    public TerminatorResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of serveur.TerminatorResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public String getXml() {
+       return "dans getXml de TerminatorRessource";
+    }
+
+    /**
+     * PUT method for updating or creating an instance of TerminatorResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    public void putXml(String content) {
     }
     
-    public Cubot2(){
-		
-                
-		System.out.println("Program started");
-		
-                
-                int retour=connexionVREP();//on se connecte a vrep
-                
-                if (retour==1){
-                
-                       initVariables();
-                       
-                       
-                       
-                       
-                       float orientationvoulue = connexionclient() ;
-                       
-                       
-                       
-                       
-                       while (getSimulationTime()<500){
-                       codeRetour = vrep.simxReadProximitySensor(clientID, handle_Sensor.getValue(),result, null, null, null, remoteApi.simx_opmode_streaming);
-                       
-                       if (result.getValue() == true) // si obstacle detecté
-                       { backUntilTime = getSimulationTime()+4;}
-                       
-                       if (backUntilTime >getSimulationTime()) // On tourne
-                       { temps=getSimulationTime()+10;
-                           vrep.simxSetJointTargetVelocity(clientID, handle_MoteurGauche.getValue(), speed/8, remoteApi.simx_opmode_blocking);
-                           vrep.simxSetJointTargetVelocity(clientID, handle_MoteurDroit.getValue(), -speed/8, remoteApi.simx_opmode_blocking);
-                       }
-                           
-                      else 
-                       {
-                           if (getSimulationTime()>temps) // on se remet dans l'axe de angles0
-                           {
-                              vrep.simxGetObjectOrientation(clientID,handle_bubbleRob.getValue(),-1,Angles, remoteApi.simx_opmode_streaming);
-                             
 
-                               if ((Angles.getArray()[2] <orientationvoulue + 0.1 ) && (Angles.getArray()[2]> orientationvoulue - 0.1))
-                               {   temps = getSimulationTime()+10;}
-                            
-                               //il tourne sur lui meme
-                            vrep.simxSetJointTargetVelocity(clientID, handle_MoteurDroit.getValue(), speed/8, remoteApi.simx_opmode_blocking);
-                            vrep.simxSetJointTargetVelocity(clientID, handle_MoteurGauche.getValue(), -speed/8, remoteApi.simx_opmode_blocking); 
-                           }
-                           
-                           else
-                           {
-                               //il va tout droit
-                             vrep.simxSetJointTargetVelocity(clientID, handle_MoteurGauche.getValue(), speed, remoteApi.simx_opmode_blocking); 
-                             vrep.simxSetJointTargetVelocity(clientID, handle_MoteurDroit.getValue(), speed, remoteApi.simx_opmode_blocking);
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/getSimulationTime")
+    private String getSimulationTime(){
+        
+        FloatWA outFloats= new FloatWA(1);
 
-                           }
-                       }
-                           
-                           
-                           
-                                       
-                       }
+		int result=vrep.simxCallScriptFunction(clientID,"bubbleRob",remoteApi.sim_scripttype_childscript, "getSimulationTime_function",null,null,null,null,null,outFloats,null,null,remoteApi.simx_opmode_blocking);
+		if (result==remoteApi.simx_return_ok) {
+			float returnValue = outFloats.getArray()[0];
+			return formatJSON("time", returnValue);
+		} else {
+			System.out.println("Remote function call to simGetSimulationTime failed : " + codeRetourDescription(result));
+			return formatJSON("time", -1);
+		}
 
-			System.out.println("fin bLAAAAAAAAAAA");
-			// Now send some data to V-REP in a non-blocking fashion:
-			vrep.simxAddStatusbarMessage(clientID, "Hello V-REP!", remoteApi.simx_opmode_oneshot);
-
-			// Before closing the connection to V-REP, make sure that the last command sent
-			// out had time to arrive. You can guarantee this with (for example):
-			IntW pingTime = new IntW(0);
-			vrep.simxGetPingTime(clientID, pingTime);
-
-			// Now close the connection to V-REP:
-			vrep.simxFinish(clientID);
-		} else{
-			System.out.println("Failed connecting to remote API server");
-		System.out.println("Program ended");
-                }
+           
     }
-                
-                
-        private int connexionVREP() {
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/connexionVREP")
+    public String connexionVREP() {
         			// Connexion a VREP
-                            vrep = new remoteApi();
-                            vrep.simxFinish(-1);
+                            //vrep = new remoteApi();
+                           /* vrep.simxFinish(-1);
                             int answer = 0;
                             int clientID = vrep.simxStart("127.0.0.1", 19999, true, true, 5000, 5);
                             if (clientID != -1) {
                                 System.out.println("Connected to remote API server");
+                            
                                 answer = 1;
                                 
-                            }
-                            return(answer);
+                            }*/
+                            return(formatJSON("ConnexionVrep",connexionVrep()));
 	}
-        
-        private void initVariables() {
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/initVariables")
+    private void initVariables() {
         			// initialisation des variables
                         codeRetour = vrep.simxGetObjects(clientID, vrep.sim_handle_all, objectHandles, vrep.simx_opmode_blocking);
 			if (codeRetour == remoteApi.simx_return_ok)
@@ -190,22 +146,11 @@ public class Cubot2 {
                         vrep.simxGetObjectOrientation(clientID,handle_bubbleRob.getValue(),-1,Angles0, remoteApi.simx_opmode_streaming);
                             
 	}
-
-        private float getSimulationTime() {
-		
-		FloatWA outFloats= new FloatWA(1);
-
-		int result=vrep.simxCallScriptFunction(clientID,"bubbleRob",remoteApi.sim_scripttype_childscript, "getSimulationTime_function",null,null,null,null,null,outFloats,null,null,remoteApi.simx_opmode_blocking);
-		if (result==remoteApi.simx_return_ok) {
-			float returnValue = outFloats.getArray()[0];
-			return returnValue;
-		} else {
-			System.out.println("Remote function call to simGetSimulationTime failed : " + codeRetourDescription(result));
-			return -1;
-		}	
-
-	}
-	private static String codeRetourDescription(int codeRetour) {
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/codeRetourDescription")
+    private static String codeRetourDescription(int codeRetour) {
 		switch (codeRetour) {
 		case remoteApi.simx_return_ok:
 			return "0:\tThe function executed fine.";
@@ -233,13 +178,14 @@ public class Cubot2 {
 		}
 
 	}
-        
-        
-        
-        protected static int connexionclient() {
+    /*
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/connexionclient")
+    protected String connexionclient() {
         			// Connexion au serveur simulant le robot théorique
                         try {
-                                 URL url = new URL("http://localhost:8084/REST_Terminator/webresources/Terminator/orientation");
+                                 URL url = new URL("http://localhost:8084/REST_Terminator/webresources/Terminator/orientationJSON");
                                     HttpURLConnection connexion = (HttpURLConnection) url.openConnection();
                                     connexion.setRequestProperty("User-Agent", "");
                                     connexion.connect();
@@ -255,61 +201,22 @@ public class Cubot2 {
                                     System.out.println("Connecté au client localhost... !"); 
                                     System.out.println("orientation = " + answer1);   
                                     connexion.disconnect();
-                                    return answer1;
+                                    
+                                    return formatJSON("connécté au client, orientation : ", answer1);
                                     
                             } 
                         
                         catch (Exception ex) {
                                             ex.printStackTrace();
                                             int answer = 0;
-                                            return answer;
+                                            return formatJSON("pas connécté au client, orientation", -1);
                         }  
-        }
-        
-}
-
-@Path("Terminator")
-class TerminatorResource {
-     
+        }*/
     
-    @Context
-    private UriInfo context;
-
-    /**
-     * Creates a new instance of TerminatorResource
-     */
-  
     
-    public TerminatorResource() {
-        
-    }
-    
-   
-    
-    //ICI c'est les methodes proposées par le serveur
-    /**
-     * Retrieves representation of an instance of serveur.Cubot2
-     * @return an instance of java.lang.String
-     */
-    @GET
-    @Produces(MediaType.APPLICATION_XML)
-    public String getXml() {
-       return "dans getXml de TerminatorRessource";
-    }
-
-    /**
-     * PUT method for updating or creating an instance of Cubot2
-     * @param content representation for the resource
-     */
-    @PUT
-    @Consumes(MediaType.APPLICATION_XML)
-    public void putXml(String content) {
-    }
-    
-
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("/orientation")
+    @Path("/orientationJSON")
     public String orientationJSON(){
 
             return formatJSON("orientation", -1);
@@ -329,8 +236,21 @@ class TerminatorResource {
         json += "}";
         return json;
     }
+     
+     public int connexionVrep() {
+        			// Connexion a VREP
+                            vrep = new remoteApi();
+                            vrep.simxFinish(-1);
+                            int answer = 0;
+                            int clientID = vrep.simxStart("127.0.0.1", 19999, true, true, 5000, 5);
+                            if (clientID != -1) {
+                                System.out.println("Connected to remote API server");
+                            
+                                answer = 1;
+                                
+                            }
+                            return(answer);
+
     
-
 }
-
-
+     }
