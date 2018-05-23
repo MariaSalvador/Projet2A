0561010eaca85bfaa76ba548cbcf02905ad2ec64
MariaSalvador <maria.salvador-masip2@etu.univ-lorraine.fr>
2018-05-23 14:00:14 +0200

    remise en forme de Terminator Resource

--- REST_Terminator/src/java/serveur/TerminatorResource.java
+++ REST_Terminator/src/java/serveur/TerminatorResource.java
@@ -20,7 +20,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
-//import org.json.JSONObject;
 /**
  * REST Web Service
  *
@@ -39,10 +38,9 @@ public class TerminatorResource {
     IntW handle_MoteurGauche = new IntW(0);
     IntW handle_Sensor = new IntW(0);
     IntW handle_bubbleRob= new IntW(0);
-  //  FloatWA Angles0 = new FloatWA(3); //angles voulus
     FloatWA Angles = new FloatWA(3); 
     FloatWA positionBubbleRob = new FloatWA(3); 
-      FloatWA pointB = new FloatWA(3); 
+    FloatWA pointB = new FloatWA(3); 
 
     int codeRetour;
     
@@ -57,42 +55,20 @@ public class TerminatorResource {
     public TerminatorResource() {
     }
 
-    /**
-     * Retrieves representation of an instance of serveur.TerminatorResource
-     * @return an instance of java.lang.String
-     */
-    @GET
-    @Produces(MediaType.APPLICATION_XML)
-    public String getXml() {
-       return "dans getXml de TerminatorRessource";
-    }
 
-    /**
-     * PUT method for updating or creating an instance of TerminatorResource
-     * @param content representation for the resource
-     */
-    @PUT
-    @Consumes(MediaType.APPLICATION_XML)
-    public void putXml(String content) {
-    }
-    
-
- 
     
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/connexionVREP")
     public int connexionVREP() {
-        			// Connexion a VREP
-                            
-                             vrep.simxFinish(-1);
-                           
-                            int clientID = vrep.simxStart("127.0.0.1", 19999, true, true, 5000, 5);
-                            if (clientID != -1) {
-                              vrep.simxAddStatusbarMessage(clientID,"HELLOOO VREP",remoteApi. simx_opmode_oneshot);    
-                            }
-                            
-                            return clientID;
+        // Connexion a VREP
+        //Returns le Client ID
+        vrep.simxFinish(-1); //Fermeture d'autres possibles connexions précédentes
+        int clientID = vrep.simxStart("127.0.0.1", 19999, true, true, 5000, 5);
+        if (clientID != -1) {
+            vrep.simxAddStatusbarMessage(clientID,"HELLO VREP",remoteApi. simx_opmode_oneshot);    //Ecrit sur la console V-REP
+        }
+        return clientID;
 	}
     
     
@@ -100,30 +76,31 @@ public class TerminatorResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/getSimulationTime")
     public String getSimulationTime(){
-        
+        //Renvoie le temps dans la simulation V-REP sous format JSON
+        //Si erreur, renvoie -1 en format JSON
         FloatWA outFloats= new FloatWA(1);
 
-		int result=vrep.simxCallScriptFunction(clientID,"bubbleRob",remoteApi.sim_scripttype_childscript, "getSimulationTime_function",null,null,null,null,null,outFloats,null,null,remoteApi.simx_opmode_blocking);
-		if (result==remoteApi.simx_return_ok) {
-			float returnValue = outFloats.getArray()[0];
-			return formatJSON("time", returnValue);
-		} else {
-			System.out.println("Remote function call to simGetSimulationTime failed : " + codeRetourDescription(result));
-			return formatJSON("time", -1);
-		}
-
-           
+	int result=vrep.simxCallScriptFunction(clientID,"bubbleRob",remoteApi.sim_scripttype_childscript, "getSimulationTime_function",null,null,null,null,null,outFloats,null,null,remoteApi.simx_opmode_blocking);
+	if (result==remoteApi.simx_return_ok) {
+		float returnValue = outFloats.getArray()[0];
+		return formatJSON("time", returnValue);
+	} else {
+		System.out.println("Remote function call to simGetSimulationTime failed : " + codeRetourDescription(result));
+		return formatJSON("time", -1);
+		}  
     }
+    
+    
     
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/getOrientation")
     public String getOrientation(){
-    vrep.simxGetObjectHandle(clientID, "bubbleRob", handle_bubbleRob, remoteApi.simx_opmode_blocking);   
-    vrep.simxGetObjectOrientation(clientID,handle_bubbleRob.getValue(),-1,Angles, remoteApi.simx_opmode_streaming);
-    double orientation =  Angles.getArray()[2];
-    System.out.println(Angles.getArray());
-    return formatJSON4("orientation", orientation);
+        //renvoie l'angle gamma des angles d'Euler de BubbleRob par rapport aux axes V-REP sous format JSON
+            vrep.simxGetObjectHandle(clientID, "bubbleRob", handle_bubbleRob, remoteApi.simx_opmode_blocking);   
+            vrep.simxGetObjectOrientation(clientID,handle_bubbleRob.getValue(),-1,Angles, remoteApi.simx_opmode_streaming);
+            double orientation =  Angles.getArray()[2];
+            return formatJSON4("orientation", orientation);
     }
  
     
@@ -131,87 +108,55 @@ public class TerminatorResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/ProximitySensor")
     public String proximitysensor(){
-    BoolW result =new BoolW(false);
-    vrep.simxGetObjectHandle(clientID, "bubbleRob_sensingNose", handle_Sensor, remoteApi.simx_opmode_blocking);
-    vrep.simxReadProximitySensor(clientID, handle_Sensor.getValue(),result, null, null, null, remoteApi.simx_opmode_streaming);
-    return formatJSON3("objetdetecte", result.getValue());
+        //Renvoie la valeur fournie par le SensorDetector de BubbleRob avec format JSON
+        //Renvoie un Boolen dans un JSON format (FALSE si pas d'obsstacle, True sinon)
+            BoolW result =new BoolW(false);
+            vrep.simxGetObjectHandle(clientID, "bubbleRob_sensingNose", handle_Sensor, remoteApi.simx_opmode_blocking);
+            vrep.simxReadProximitySensor(clientID, handle_Sensor.getValue(),result, null, null, null, remoteApi.simx_opmode_streaming);
+            return formatJSON3("objetdetecte", result.getValue());
     }
     
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/setMotor")
     public int  setspeedmotor(@QueryParam("moteur") String moteur, @QueryParam("valeur") float valeur){
-        if ("gauche".equals(moteur)){
-        vrep.simxGetObjectHandle(clientID, "bubbleRob_leftMotor", handle_MoteurGauche, remoteApi.simx_opmode_blocking);
-        vrep.simxSetJointTargetVelocity(clientID, handle_MoteurGauche.getValue(), valeur, remoteApi.simx_opmode_oneshot);
+        //Return 1 tourjours
+        //Change les valeurs des moteurs de BubbleRob
+            if ("gauche".equals(moteur)){
+                vrep.simxGetObjectHandle(clientID, "bubbleRob_leftMotor", handle_MoteurGauche, remoteApi.simx_opmode_blocking);
+                vrep.simxSetJointTargetVelocity(clientID, handle_MoteurGauche.getValue(), valeur, remoteApi.simx_opmode_oneshot);
                 }
-        else {
-        vrep.simxGetObjectHandle(clientID, "bubbleRob_rightMotor", handle_MoteurDroit, remoteApi.simx_opmode_blocking);
-        vrep.simxSetJointTargetVelocity(clientID, handle_MoteurDroit.getValue(), valeur, remoteApi.simx_opmode_blocking);}
-        return 1;
-    }
+            else {
+                vrep.simxGetObjectHandle(clientID, "bubbleRob_rightMotor", handle_MoteurDroit, remoteApi.simx_opmode_blocking);
+                vrep.simxSetJointTargetVelocity(clientID, handle_MoteurDroit.getValue(), valeur, remoteApi.simx_opmode_blocking);}
+            return 1;
+        }
     
-        @GET
+    @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/getPositionDestination")
-    public String Trajectoire(){
-  
-    vrep.simxGetObjectHandle(clientID, "POINT_B", handle_destination, remoteApi.simx_opmode_blocking);
-    vrep.simxGetObjectPosition(clientID, handle_destination.getValue(),-1, pointB, remoteApi.simx_opmode_streaming);
-   double x =  pointB.getArray()[0];
-    double y = pointB.getArray()[1];
-    return formatJSON2(x, y);
+    public String PositionDestination(){
+            //Renvoie les coordonnées [x, y] de la destination de BubbleRob en format JSON
+            //La destination doit s'appeler POINT_B sur la scène V-REP !
+            vrep.simxGetObjectHandle(clientID, "POINT_B", handle_destination, remoteApi.simx_opmode_blocking);
+            vrep.simxGetObjectPosition(clientID, handle_destination.getValue(),-1, pointB, remoteApi.simx_opmode_streaming);
+            double x =  pointB.getArray()[0];
+            double y = pointB.getArray()[1];
+            return formatJSON2(x, y);
     }
     
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/getPositionBubbleRob")
     public String PositionBubbleRob(){
+            //Renvoie les coordonnées [x, y] de BubbleRob en format JSON
+            vrep.simxGetObjectHandle(clientID, "bubbleRob", handle_bubbleRob, remoteApi.simx_opmode_blocking);
+            vrep.simxGetObjectPosition(clientID, handle_bubbleRob.getValue(),-1, positionBubbleRob, remoteApi.simx_opmode_streaming);
+            double x =  positionBubbleRob.getArray()[0];
+            double y = positionBubbleRob.getArray()[1];
+            return formatJSON2(x, y);
+        }
 
-    vrep.simxGetObjectHandle(clientID, "bubbleRob", handle_bubbleRob, remoteApi.simx_opmode_blocking);
-    vrep.simxGetObjectPosition(clientID, handle_bubbleRob.getValue(),-1, positionBubbleRob, remoteApi.simx_opmode_streaming);
-    double x =  positionBubbleRob.getArray()[0];
-    double y = positionBubbleRob.getArray()[1];
-    return formatJSON2(x, y);
-    }
-
-
-
-  /*  @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("/initVariables")
-    private void initVariables() {
-        			// initialisation des variables
-                        codeRetour = vrep.simxGetObjects(clientID, vrep.sim_handle_all, objectHandles, vrep.simx_opmode_blocking);
-			if (codeRetour == remoteApi.simx_return_ok)
-				System.out.format("Number of objects in the scene: %d\n", objectHandles.getArray().length);
-			else
-				System.out.format("simxGetObjects call returned with error code: " + codeRetourDescription(codeRetour));
-                        
-                        codeRetour = vrep.simxGetObjectHandle(clientID, "bubbleRob_rightMotor", handle_MoteurDroit, remoteApi.simx_opmode_blocking);
-			if (codeRetour == remoteApi.simx_return_ok)
-				System.out.println("handle MoteurDroit = " + handle_MoteurDroit.toString());
-
-			codeRetour = vrep.simxGetObjectHandle(clientID, "bubbleRob_leftMotor", handle_MoteurGauche, remoteApi.simx_opmode_blocking);
-			if (codeRetour == remoteApi.simx_return_ok)
-				System.out.println("handle MoteurGauche = " + handle_MoteurGauche.toString());
-                       
-                        codeRetour = vrep.simxGetObjectHandle(clientID, "bubbleRob_sensingNose", handle_Sensor, remoteApi.simx_opmode_blocking);
-			if (codeRetour == remoteApi.simx_return_ok)
-				System.out.println("handle Sensor = " + handle_Sensor.toString());
-                        
-                        codeRetour = vrep.simxGetObjectHandle(clientID, "bubbleRob", handle_bubbleRob, remoteApi.simx_opmode_blocking);
-			if (codeRetour == remoteApi.simx_return_ok)
-				System.out.println("handle bubbleRob = " + handle_bubbleRob.toString());
-
-			try {
-				Thread.sleep(2000);
-			} catch (InterruptedException ex) {
-				Thread.currentThread().interrupt();
-			}
-                        vrep.simxGetObjectOrientation(clientID,handle_bubbleRob.getValue(),-1,Angles0, remoteApi.simx_opmode_streaming);
-                            
-	}*/
     
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -248,11 +193,14 @@ public class TerminatorResource {
 
     
     protected String formatJSON(String nom,  float resultat) {
+        //Format JSON avec un float
+        //On peut passer en paramètre le nom et le float associé
         String json = "{";
         json += "\"" + nom + "\":" + resultat + "}";
         return json;
     }
      protected String formatJSON2(double x,   double y) {
+        //Format JSON avec deux doubles [x,y]
         String json = "{\n";
         json += "\t\t\"x\": \"" + x + "\",\n";
         json += "\t\t\"y\": \"" + y+ "\",\n";
@@ -262,6 +210,8 @@ public class TerminatorResource {
     }
      
      protected String formatJSON3(String nom,  Boolean resultat) {
+        //Format JSON avec un Booléan
+        //On peut passer en paramètre le nom et le Booléan associé
         String json = "{";
         json += "\"" + nom + "\":" + resultat + "}";
  
@@ -269,24 +219,10 @@ public class TerminatorResource {
     }
      
      protected String formatJSON4(String nom,  double resultat) {
+         //Format JSON avec un double
+        //On peut passer en paramètre le nom et le Booléan associé
         String json = "{";
         json += "\"" + nom + "\":" + resultat + "}";
         return json;
     }
-    /* public int connexionVrep() {
-        			// Connexion a VREP
-                            vrep = new remoteApi();
-                            vrep.simxFinish(-1);
-                            int answer = 0;
-                            int clientID = vrep.simxStart("127.0.0.1", 19999, true, true, 5000, 5);
-                            if (clientID != -1) {
-                                System.out.println("Connected to remote API server");
-                            
-                                answer = 1;
-                                
-                            }
-                            return(answer);
-
-    
-}*/
 }
